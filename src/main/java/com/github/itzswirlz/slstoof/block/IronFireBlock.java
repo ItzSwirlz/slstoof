b248134cda88c18f11df7adba823057ebd674638
Joshua Peisach <itzswirlz2020@outlook.com>
2025-01-30 12:10:51 -0500

    Fix fire blocks not disappearing after base block is removed
    This is what that deprecated function was about I guess

--- src/main/java/com/github/itzswirlz/slstoof/block/IronFireBlock.java
+++ src/main/java/com/github/itzswirlz/slstoof/block/IronFireBlock.java
@@ -7,8 +7,9 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.world.WorldAccess;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.tick.ScheduledTickView;
 
 public class IronFireBlock extends AbstractFireBlock {
     public static final MapCodec<IronFireBlock> CODEC = createCodec(IronFireBlock::new);
@@ -21,11 +22,8 @@ public class IronFireBlock extends AbstractFireBlock {
         return CODEC;
     }
 
-    // This is deprecated (in AbstractBlock) but its needed to ensure if the block
-    // below the fire breaks, we remove it.
-    // SoulFireBlock uses it though?
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState,
-            WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+    protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView,
+            BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
         return this.canPlaceAt(state, world, pos) ? this.getDefaultState() : Blocks.AIR.getDefaultState();
     }
 
--- src/main/java/com/github/itzswirlz/slstoof/block/RedstoneFireBlock.java
+++ src/main/java/com/github/itzswirlz/slstoof/block/RedstoneFireBlock.java
@@ -7,9 +7,10 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.tick.ScheduledTickView;
 
 public class RedstoneFireBlock extends AbstractFireBlock {
     public static final MapCodec<RedstoneFireBlock> CODEC = createCodec(RedstoneFireBlock::new);
@@ -22,11 +23,8 @@ public class RedstoneFireBlock extends AbstractFireBlock {
         return CODEC;
     }
 
-    // This is deprecated (in AbstractBlock) but its needed to ensure if the block
-    // below the fire breaks, we remove it.
-    // SoulFireBlock uses it though?
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState,
-            WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+    protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView,
+            BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
         return this.canPlaceAt(state, world, pos) ? this.getDefaultState() : Blocks.AIR.getDefaultState();
     }
 
