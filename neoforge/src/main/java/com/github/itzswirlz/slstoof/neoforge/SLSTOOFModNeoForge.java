fb70c23daa20c6dd36696de20964f34dcd9bc697
Joshua Peisach <itzswirlz2020@outlook.com>
2025-02-24 19:28:48 -0500

    Add neoforge files (up next: registry refactor)

---
+++ neoforge/src/main/java/com/github/itzswirlz/slstoof/neoforge/SLSTOOFModNeoForge.java
@@ -0,0 +1,13 @@
+package com.github.itzswirlz.slstoof.neoforge;
+
+import com.github.itzswirlz.slstoof.SLSTOOFMod;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(SLSTOOFMod.MOD_ID)
+public final class SLSTOOFModNeoForge {
+    public SLSTOOFModNeoForge() {
+        // Run our common setup.
+        SLSTOOFMod.init();
+    }
+}
