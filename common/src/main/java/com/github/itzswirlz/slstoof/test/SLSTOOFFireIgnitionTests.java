8166febc262afcce9acf7c0e4b7dd11efc1bdd15
Joshua Peisach <itzswirlz2020@outlook.com>
2025-03-24 11:45:09 -0400

    Port to 1.21.5-rc2

--- common/src/main/java/com/github/itzswirlz/slstoof/test/SLSTOOFFireIgnitionTests.java
+++ common/src/main/java/com/github/itzswirlz/slstoof/test/SLSTOOFFireIgnitionTests.java
@@ -5,21 +5,24 @@ import net.minecraft.test.TestContext;
 import net.minecraft.util.math.BlockPos;
 
 public class SLSTOOFFireIgnitionTests {
+	// Commented the .complete() as it may be unnecessary - in fact, it could be
+	// causing testcases that should fail to pass
+	// see https://report.bugs.mojang.com/servicedesk/customer/portal/2/MC-295787
 	public static void testCopperFireIgnition(TestContext context) {
 		context.pushButton(0, 1, 0);
 		context.expectBlockAtEnd(SLSTOOFBlocks.COPPER_FIRE.get(), new BlockPos(1, 2, 1));
-		context.complete();
+//		context.complete();
 	}
 
 	public static void testIronFireIgnition(TestContext context) {
 		context.pushButton(0, 1, 0);
 		context.expectBlockAtEnd(SLSTOOFBlocks.IRON_FIRE.get(), new BlockPos(1, 2, 1));
-		context.complete();
+//		context.complete();
 	}
 
 	public static void testRedstoneFireIgnition(TestContext context) {
 		context.pushButton(0, 1, 0);
 		context.expectBlockAtEnd(SLSTOOFBlocks.REDSTONE_FIRE.get(), new BlockPos(1, 2, 1));
-		context.complete();
+//		context.complete();
 	}
 }
